69eeb7439e3906fedad60de3613aa4c623501389
Omar-Ghon <o.ghoneim04@gmail.com>
2024-02-15 15:59:55 -0500

    Created Translator class to encapsulate JSONObject response in object (close #39)

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team111/Translator.java
@@ -0,0 +1,12 @@
+package ca.mcmaster.se2aa4.island.team111;
+
+import org.json.JSONObject;
+
+public class Translator {
+
+    public Information translate(JSONObject response) {
+        Information I = new Information(response.getInt("cost"), response.getJSONObject("extras"));
+        return I;
+    }
+
+}
