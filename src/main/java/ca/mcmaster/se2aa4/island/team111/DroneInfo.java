a64d8e15b33ee1788d6961ffe09c8f7731f74734
Omar-Ghon <o.ghoneim04@gmail.com>
2024-02-15 16:55:03 -0500

    DroneInfo interface to be realized by Drone (close #42)

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team111/DroneInfo.java
@@ -0,0 +1,6 @@
+package ca.mcmaster.se2aa4.island.team111;
+
+public interface DroneInfo {
+    int checkBattery(); //Battery checkBattery when Battery class implemented
+    void currentDirection(); //changes to Direction type when Direction enum implemented
+}
