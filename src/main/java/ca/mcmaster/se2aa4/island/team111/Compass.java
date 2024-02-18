8a7333c61ce8063714a2dca14c881bf8a067d4d2
markou <markou@LAPTOP-7172OM1I>
2024-02-17 23:06:50 -0500

    Implements Compass enum

--- src/main/java/ca/mcmaster/se2aa4/island/team111/Compass.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team111/Compass.java
@@ -1,28 +1,28 @@
 package ca.mcmaster.se2aa4.island.team111;
 
-public interface Compass {
-    enum Turn{L,R,F};
-    default String headingRotate(String heading, Turn turn){
-        String newHeading = heading;
-        switch(turn){
-            case L:
-                switch(heading){
-                    case "N":newHeading = "W"; break;
-                    case "W":newHeading = "S"; break;
-                    case "S":newHeading = "E"; break;
-                    case "E":newHeading = "N"; break;
-                    default: break;
-                }
-            case R:
-                switch(heading){
-                    case "N":newHeading = "E"; break;
-                    case "W":newHeading = "N"; break;
-                    case "S":newHeading = "W"; break;
-                    case "E":newHeading = "S"; break;
-                    default: break;
-                }
-            default: break;
+public enum Compass 
+{
+    NORTH,
+    WEST,
+    SOUTH,
+    EAST;
+
+    public Compass turnRight(Compass heading){
+        switch(heading){
+            case NORTH: return Compass.EAST;
+            case WEST: return Compass.NORTH;
+            case SOUTH: return Compass.WEST;
+            case EAST: return Compass.SOUTH;
+            default: return heading;
         }
-        return newHeading;
+    }
+    public Compass turnLeft(Compass heading){
+        switch(heading){
+            case NORTH: return Compass.WEST;
+            case WEST: return Compass.SOUTH;
+            case SOUTH: return Compass.EAST;
+            case EAST: return Compass.NORTH;
+            default: return heading;
+        }
     }
 }
