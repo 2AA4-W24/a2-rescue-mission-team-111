3f023b695e00f0c0345076f61b46cd7a8eac2098
Omar-Ghon <o.ghoneim04@gmail.com>
2024-03-20 15:35:43 -0400

    Abstraction leak from public GridSearcher methods fixed by putting states as internal classes (close #114)

--- src/main/java/ca/mcmaster/se2aa4/island/team111/Drone.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team111/Drone.java
@@ -1,15 +1,19 @@
 package ca.mcmaster.se2aa4.island.team111;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
 
 public class Drone {
+
+    private final Logger logger = LogManager.getLogger();
 
     private Battery battery;
     private Position pos = new Position(0, 0);
     private Compass direction;
     private DroneState current_state = DroneState.FINDING;
     private EchoArrive e1 = new EchoArrive();
-    private GridSearcher g1 = new GridSearcher(Compass.NORTH);
+    private GridSearcher g1;
     private Information current_info = new Information(0, new JSONObject());
     private String magicWord = "action";
 
@@ -17,19 +21,17 @@ public class Drone {
         this.battery = new Battery(charge);
         Compass compass = Compass.NORTH;
         this.direction = compass.StoC(dir);
-        this.g1 = new GridSearcher(direction);
     }
     
     //Receive info from acknowledge results here
     public void receiveInfo(Information I) {
         current_info = I;
-        g1.updateInfo(I, pos);
         battery.depleteCharge(I.getCost());
     }
 
     public JSONObject giveDecision() {
         JSONObject decision = new JSONObject();
-
+        logger.info("Battery: " + battery.getCharge());
         //If battery is low, stop
         if (battery.isLow()) {
             decision.put(magicWord, "stop");
@@ -44,6 +46,7 @@ public class Drone {
                     Compass old_dir = direction;
                     direction = direction.StoC(parameters.getString("direction"));
                     pos = pos.changePositionTurn(old_dir, direction);
+                    this.g1 = new GridSearcher(old_dir, direction);
                 } else if (decision.get(magicWord) == "fly") {
                     pos = pos.changePositionFly(direction);
                 }
@@ -63,11 +66,10 @@ public class Drone {
                 }
                 if (e1.arrivingDone()) {
                     current_state = current_state.nextState();
-                    g1.setDirBeforeTurn(direction);
                 }
                 return decision;
             case SEARCHING:
-                decision = g1.performSearch();
+                decision = g1.performSearch(current_info, pos);
                 if (decision.get(magicWord) == "heading") {
                     JSONObject parameters = decision.getJSONObject("parameters");
                     Compass old_dir = direction;
