7476a8f1813e902eab32098725e7e35b06994a14
Mazen <mazenhussain01@gmail.com>
2024-03-22 22:31:34 -0400

    Optimized battery test

--- src/test/java/ca/mcmaster/se2aa4/island/team111/BatteryTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team111/BatteryTest.java
@@ -3,19 +3,26 @@ package ca.mcmaster.se2aa4.island.team111;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+
 
 class BatteryTest {
 
+    private Battery bat;
+
+    @BeforeEach
+    void init(){
+        bat = new Battery(10);
+    }
+
     @Test
     void checkLow() {
-        Position pos = new Position(5000, 0); //Some far away position
-        Battery bat = new Battery(10);
+        Position pos = new Position(5000, 0); //Some far away position 
         assertEquals(true,bat.isLow(pos));
     }
 
     @Test
-    void depleteTest(){
-        Battery bat = new Battery(10);
+    void depleteTest(){ 
         bat.depleteCharge(5);
         assertEquals(5,bat.getCharge());
     }
