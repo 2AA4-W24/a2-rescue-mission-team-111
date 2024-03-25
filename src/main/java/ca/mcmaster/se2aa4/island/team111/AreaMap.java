48c629e647ce6cacc31c33bd61a96e240b0d9ba6
Mazen <mazenhussain01@gmail.com>
2024-03-24 21:52:52 -0400

    Documentation of AreaMap for maintenance

--- src/main/java/ca/mcmaster/se2aa4/island/team111/AreaMap.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team111/AreaMap.java
@@ -7,20 +7,24 @@ public class AreaMap {
     private List<POI> creeks;
     private POI site;
 
+    //AreaMap contains map of positions of the map associated with their positions
+
     public AreaMap(Map<Position, List<String>> biomeMap, List<POI> creekList, POI emergencySite) {
         this.biomes = biomeMap;
         this.creeks = creekList;
         this.site = emergencySite;
     }
 
+    //Useful for future rescue actions
     public Map<Position, List<String>> getBiomes() {
         return biomes;
     }
 
+    //Calculates the closest creek to the site
     public String calculateClosest() {
         if (creeks.isEmpty()) {
             return "No creeks found";
-        } else if (site.getID().equals("null")) {
+        } else if (site.getID().equals("null")) { //if site not found
             POI lastCreek = creeks.get(creeks.size()-1);
             return lastCreek.getID();
         }
@@ -35,7 +39,7 @@ public class AreaMap {
         return closestCreek.getID();
     }
 
-    //Uses pythagorean mathematics to check distance
+    //Uses euclidean distance 
     public double getDistance(POI creek) {
         double x = Math.abs(site.getXvalue()-creek.getXvalue());
         double y = Math.abs(site.getYvalue()-creek.getYvalue());
